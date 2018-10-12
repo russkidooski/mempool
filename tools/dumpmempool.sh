7b29a846abd8445f22f5e8d43f7c4deea35e5f6e
russkidooski <russkidooski@gmail.com>
2018-10-12 16:46:29 -0500

    rebranded

--- tools/dumpmempool.sh
+++ tools/dumpmempool.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BITCOINCLI=~bitcoind/bin/bitcoin-cli
+BITCOINCLI=~ravend/bin/raven-cli
 
 $BITCOINCLI getrawmempool | perl -ne '/"([0-9a-f]+)"/ && print "$1\n"' | \
 while read i;  do
